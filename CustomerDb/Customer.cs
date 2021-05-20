a62961b9fa71c021ebdaeff211708ada9cb0b47f
Sarah George <sgeorge@tql.com>
2021-05-20 14:09:02 -0400

    Add project files.

---
+++ CustomerDb/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerDb
+{
+    class Customer
+    {
+        public int Id { get; private set; }
+        public string Name { get; set;}
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal Sales { get; private set; }
+        public bool Active { get; set; } = true; // have to set to true because default to false
+
+        private static int LastId { get; set; } = 0;
+
+        public void AddSales(decimal amount) // this is a method if it doesnt return anything add void. give it a name and then add the parameters
+        {
+            Sales = Sales + amount;
+
+           
+        }
+
+        public Customer(string name, string city = "Cincinnati", string state = "OH") // this is the class constructor and will set initial values of your properties
+        {
+            this.Name = name;
+            this.City = city;
+            this.State = state;
+            LastId++;
+            this.Id = LastId;
+        }
+
+
+    }
+
+    
+
+        
+        
+            
+            
+       
+
+        
+    
+}
---
+++ CustomerDb/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomerDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ord1 = new Order();
+            var n = ord1.Add1(5);
+            Console.WriteLine($"Add1 evaluates to {ord1.Add1(5)}.");
+
+
+            var cus1 = new Customer("TQL", "Cincinnati", "OH");
+            cus1.AddSales(1000);
+            cus1.AddSales(4000);
+            Console.WriteLine($"Customer {cus1.Name} sales are ${cus1.Sales}.");
+
+
+            
+           
+        }
+    }
+}
